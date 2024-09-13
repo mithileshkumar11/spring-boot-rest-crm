b5e32ccf4072a486bd67ac9a6ff12fc680db4f7a
Mithilesh Kumar <35111020+mithileshkumar11@users.noreply.github.com>
2024-09-13 19:07:41 +0530

    Update User.java

--- src/main/java/com/crm/model/User.java
+++ src/main/java/com/crm/model/User.java
@@ -44,7 +44,7 @@ public class User implements UserDetails {
     
     private Long mobile;
     
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
     
